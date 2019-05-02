1d643880ecb5f4912b9df7edc06d61f012e992c1
Luke Schlather <luke.schlather@strivr.com>
2019-05-02 09:23:32 -0700

    add lowercase ctrl-v

--- src/WindowsPowerShell/profile.ps1
+++ src/WindowsPowerShell/profile.ps1
@@ -3,7 +3,7 @@ if ($host.Name -eq 'ConsoleHost')
     Import-Module PSReadLine
     Set-PSReadLineOption -EditMode Emacs
     Set-PSReadLineKeyHandler -Key 'Ctrl+V' -Function Paste
-    Set-PSReadLineKeyHandler -Key 'Ctrl+V' -Function Paste
+    Set-PSReadLineKeyHandler -Key 'Ctrl+v' -Function Paste
     Set-PSReadLineKeyHandler -Key 'Ctrl+Backspace' -Function BackwardKillWord
     Set-PSReadLineKeyHandler -Key 'Ctrl+LeftArrow' -Function BackwardWord
     Set-PSReadLineKeyHandler -Key 'Ctrl+RightArrow' -Function ForwardWord
