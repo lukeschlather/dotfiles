92b7298805ee30581a245ed94897ca6efe89decd
Luke Schlather <luke.schlather@strivr.com>
2019-05-14 13:37:30 -0700

    add undo and which

--- src/WindowsPowerShell/profile.ps1
+++ src/WindowsPowerShell/profile.ps1
@@ -4,7 +4,10 @@ if ($host.Name -eq 'ConsoleHost')
     Set-PSReadLineOption -EditMode Emacs
     Set-PSReadLineKeyHandler -Key 'Ctrl+V' -Function Paste
     Set-PSReadLineKeyHandler -Key 'Ctrl+v' -Function Paste
+    Set-PSReadLineKeyHandler -Key 'Ctrl+/' -Function Undo
     Set-PSReadLineKeyHandler -Key 'Ctrl+Backspace' -Function BackwardKillWord
     Set-PSReadLineKeyHandler -Key 'Ctrl+LeftArrow' -Function BackwardWord
     Set-PSReadLineKeyHandler -Key 'Ctrl+RightArrow' -Function ForwardWord
 }
+
+New-Alias which get-command
