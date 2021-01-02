adca04ea20a0594e12fe5d5683edcb374984c84a
Luke Schlather <luke.schlather@gmail.com>
2021-01-02 12:40:44 -0800

    fix git config to use --global and add symlink for package

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
+New-Item -Path $PROFILE -ItemType SymbolicLink -Value "$home\dotfiles\src\WindowsPowerShell\profile.ps1" -Force
+
 New-Item -Path "$home\Documents\WindowsPowerShell\profile.ps1" -ItemType SymbolicLink -Value "$home\dotfiles\src\WindowsPowerShell\profile.ps1" -Force
 
 New-Item -Path "$env:appdata\.emacs" -ItemType SymbolicLink -Value "$home\dotfiles\primary\.emacs" -Force
@@ -6,4 +8,4 @@ New-Item -Path "$env:appdata\.autoinsert" -ItemType SymbolicLink -Value "$home\d
 New-Item -Path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\ljfs.ahk" `
          -ItemType SymbolicLink -Value "$home\dotfiles\src\autohotkey\ljfs.ahk"  -Force
 
-git config --add core.excludesfile $home\dotfiles\primary\.gitignore
+git config --global --add core.excludesfile $home\dotfiles\primary\.gitignore
